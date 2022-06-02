b369e3eea61d70eeb1b8d05caa4a92236100f3c8
Peter Schaldenbrand <pls21@pitt.edu>
2022-06-02 19:57:20 -0400

    small parameter updates

--- scripts/plan_all_strokes.py
+++ scripts/plan_all_strokes.py
@@ -116,64 +116,13 @@ def get_image_augmentation(use_normalized_clip):
 
 # Loading Strokes
 
-# def load_brush_strokes(opt, scale_factor=4):
-#     # Strokes are the individual strokes pasted onto the middle of a full sized canvas
-#     with open(os.path.join(opt.cache_dir, 'strokes_centered.npy'), 'rb') as f:
-#         strokes = pickle.load(f, encoding="latin1")
-
-
-#     #h, w = int(1024/scale_factor), int((10/8)*1024/scale_factor)
-#     h, w = int(opt.CANVAS_HEIGHT_PIX/scale_factor), int(opt.CANVAS_WIDTH_PIX/scale_factor)
-#     canvas = torch.zeros((h, w, 3))
-#     # strokes = [torch.from_numpy(s) for s in strokes]
-#     strokes_processed = []
-#     for s in strokes:
-#         s = s[:,:,3] # Just the alpha channel is necessary
-
-#         # Scale it for speed
-#         s = cv2.resize(s, (int(s.shape[1]/scale_factor), int(s.shape[0]/scale_factor)))
-
-#         # Pad the stroke so its on an image the size of a canvas and the start of the stroke is centered
-#         padX = int((w - s.shape[1])/2)
-#         padY = int((h - s.shape[0])/2)
-#         s = np.pad(s, [(padY,padY), (padX,padX)], 'constant')
-#         t = np.zeros((s.shape[0], s.shape[1], 4), dtype=np.uint8)
-
-#         t[:,:,3] = s.astype(np.uint8)
-#         t[:,:,2] = 200 # Some color for fun
-#         t[:,:,1] = 120
-
-#         strokes_processed.append(torch.from_numpy(t).float().to(device) / 255.)
-
-#     for i in range(len(strokes_processed)):
-#         # show_img(strokes[i])
-#         show_img(strokes_processed[i])
-#         # show_img(strokes_processed[i][:,:,-1])
-#         # print(strokes_processed[i][:,:,0].max())
-#     strokes = strokes_processed
-
-
 def load_brush_strokes(opt, scale_factor=4):
     with open(os.path.join(opt.cache_dir, 'strokes_centered.npy'), 'rb') as f:
         strokes = pickle.load(f, encoding="latin1")
 
-    # h, w = int(strokes[0].shape[0]/scale_factor), int(strokes[0].shape[1]/scale_factor)
-    # canvas = torch.zeros((h, w, 3))
-
     strokes_processed = []
     for s in strokes:
         s = cv2.resize(s, (int(s.shape[1]/scale_factor), int(s.shape[0]/scale_factor)))
-
-        # padX = int((w - s.shape[1])/2)
-        # padY = int((h - s.shape[0])/2)
-
-        # # In case of odd numbers
-        # xtra_x, xtra_y = w - (padX*2+s.shape[1]), h - (padY*2+s.shape[0])
-        # s = np.pad(s, [(padY,padY+xtra_y), (padX,padX+xtra_x)], 'constant')
-        # t = np.zeros((s.shape[0], s.shape[1], 4), dtype=np.uint8)
-        # t[:,:,3] = s.astype(np.uint8)
-        # t[:,:,2] = 200 # Some color for fun
-        # t[:,:,1] = 120
 
         strokes_processed.append(torch.from_numpy(s).float().to(device) / 255.)
     return strokes_processed
@@ -182,91 +131,6 @@ strokes_small = None#load_brush_strokes(scale_factor=3)
 strokes_full = None#load_brush_strokes(scale_factor=1)
 
 
-# def next_stroke(painting, target, x_y_attempts=2):
-#     # Plan a single brush stroke
-#     with torch.no_grad():
-#         l1_loss = nn.L1Loss()
-#         canvas = painting#painting(strokes=strokes_small)
-#         diff = torch.mean(torch.abs(canvas - target).unsqueeze(dim=0), dim=2)
-#         #show_img(diff)
-#         diff /= diff.sum() # Prob distribution
-
-#         diff = diff.detach().cpu()[0][0].numpy()
-
-#     opt_params = { # Find the optimal parameters
-#         'x':None, 'y':None, 'rot':None,
-#         'stroke':None, 'canvas':None, 'loss':9999999,
-#         'stroke_ind':None, 'stroke_bool_map':None,
-#     }
-
-#     for x_y_attempt in range(x_y_attempts):
-#         y, x = np.unravel_index(np.random.choice(len(diff.flatten()), p=diff.flatten()), diff.shape)
-#         color = target[0,:,y,x]
-#         h, w = strokes_small[0].shape[0], strokes_small[0].shape[1]
-#         y = torch.from_numpy(np.array(y))/h*2 - 1
-#         x = torch.from_numpy(np.array(x))/w*2 - 1
-
-#         for stroke_ind in range(len(strokes_small)):
-#             # for rot in range(0, 360, 45):
-#             #     brush_stroke = BrushStroke(stroke_ind, color=color, a=rot/(3.14*2), xt=x, yt=y).to(device)
-#             #     single_stroke = brush_stroke(strokes=strokes_small)
-#             #     #print(canvas.shape, single_stroke[:,3:].shape)
-#             #     canvas_candidate = canvas * (1 - single_stroke[:,3:]) + single_stroke[:,3:] * single_stroke[:,:3]
-
-#             #     #loss = loss_fcn(canvas_candidate, target) + nn.L1Loss()(canvas_candidate, target)
-#             #     loss = l1_loss(canvas_candidate, target)
-
-#             #     if loss < opt_params['loss']:
-#             #         opt_params['x'], opt_params['y'] = x, y
-#             #         opt_params['rot'] = rot
-#             #         opt_params['canvas'] = canvas_candidate
-#             #         opt_params['loss'] = loss
-#             #         opt_params['stroke_ind'] = stroke_ind
-#             #         opt_params['brush_stroke'] = brush_stroke
-#             brush_stroke = BrushStroke(stroke_ind, color=color, a=None, xt=x, yt=y).to(device)
-#             opt = torch.optim.Adam(brush_stroke.parameters(), lr=4e-2)
-#             for brush_opt_iter in range(10):
-#                 opt.zero_grad()
-#                 single_stroke = brush_stroke(strokes=strokes_small)
-#                 canvas_candidate = canvas * (1 - single_stroke[:,3:]) + single_stroke[:,3:] * single_stroke[:,:3]
-#                 loss = 0
-#                 loss += nn.L1Loss()(canvas_candidate, target)
-#                 #loss += clip_conv_loss(canvas_candidate, target).item()*0.2
-#                 loss.backward()
-#                 opt.step()
-#             if loss < opt_params['loss']:
-#                 opt_params['canvas'] = canvas_candidate
-#                 opt_params['loss'] = loss
-#                 opt_params['stroke_ind'] = stroke_ind
-#                 opt_params['brush_stroke'] = brush_stroke
-#         #print(opt_params['loss'])
-#         return opt_params['brush_stroke'], opt_params['canvas']
-
-# def purge_extraneous_strokes(painting, target, thresh=0):
-#     # Remove strokes that don't help the L1 loss
-#     l1_loss = nn.L1Loss()
-#     with torch.no_grad():
-#         canvas_with_stroke = painting(strokes=strokes_small)
-#         i = 0
-#         while i < len(painting.brush_strokes):
-#             removed_stroke = painting.brush_strokes[i]
-#             painting.brush_strokes = painting.brush_strokes[0:i] + painting.brush_strokes[i+1:]
-#             canvas_without_stroke = painting(strokes=strokes_small)
-#             loss_with_stroke = l1_loss(canvas_with_stroke, target)
-#             loss_without_stroke = l1_loss(canvas_without_stroke, target)
-
-#             if loss_with_stroke < loss_without_stroke:
-#                 # Don't purge stroke. Put stroke back and move on
-#                 painting.brush_strokes.insert(i, removed_stroke)
-#                 i+=1
-#             else:
-#                 # Purge it
-#                 canvas_with_stroke = canvas_without_stroke
-#     return painting
-
-
-
-#############################################################
 
 def next_stroke(canvas, target, x_y_attempts=10):
     with torch.no_grad():
@@ -346,10 +210,15 @@ def relax(painting, target, batch_size=20):
                     canvas_after = canvas_after * (1 - single_stroke[:,3:]) + single_stroke[:,3:] * single_stroke
 
         brush_stroke = painting.brush_strokes[i]
-        best_stroke, best_loss = brush_stroke, 999999
+        best_stroke = copy.deepcopy(brush_stroke)
+        single_stroke = brush_stroke(strokes_small)
+        canvas = canvas_before * (1 - single_stroke[:,3:]) + single_stroke[:,3:] * single_stroke
+        canvas = canvas * (1 - canvas_after[:,3:]) + canvas_after[:,3:] * canvas_after
+        best_loss = loss_fcn(canvas, target, use_clip_loss=False)
+
         for stroke_ind in range(len(strokes_small)):
             brush_stroke.stroke_ind = stroke_ind
-            opt = torch.optim.Adam(brush_stroke.parameters(), lr=5e-3)
+            opt = torch.optim.Adam(brush_stroke.parameters(), lr=1e-3)
             for it in range(10):
                 opt.zero_grad()
                 single_stroke = brush_stroke(strokes_small)
@@ -370,6 +239,13 @@ def relax(painting, target, batch_size=20):
 
         canvas_before = canvas_before * (1 - single_stroke[:,3:]) + single_stroke[:,3:] * single_stroke
         relaxed_brush_strokes.append(brush_stroke)
+
+        global local_it
+        local_it +=1
+        if i % 5 == 0:
+            relaxed_painting = copy.deepcopy(painting)
+            relaxed_painting.brush_strokes = nn.ModuleList(relaxed_brush_strokes).extend(painting.brush_strokes[i+1:])
+            log_progress(relaxed_painting, force_log=True)
 
     relaxed_painting = copy.deepcopy(painting)
     relaxed_painting.brush_strokes = nn.ModuleList(relaxed_brush_strokes)
@@ -404,7 +280,7 @@ def purge_extraneous_brush_strokes(painting, target):
         loss_without_stroke = nn.L1Loss()(canvas_without_stroke[:,:3], target)
         loss_with_stroke = nn.L1Loss()(canvas_with_stroke[:,:3], target)
 
-        if loss_with_stroke + 0.001 < loss_without_stroke:
+        if loss_with_stroke + 0.0 < loss_without_stroke:
             # Keep the stroke
             canvas_before = canvas_before * (1 - single_stroke[:,3:]) + single_stroke[:,3:] * single_stroke
             relaxed_brush_strokes.append(brush_stroke)
@@ -415,10 +291,12 @@ def purge_extraneous_brush_strokes(painting, target):
 
 
 def discretize_colors(painting, discrete_colors):
-    pass
-    # with torch.no_grad():
-    #     for brush_stroke in painting.brush_strokes:
-    #         brush_stroke.color_transform[:] = discretize_color(brush_stroke, discrete_colors)
+    # pass
+    with torch.no_grad():
+        for brush_stroke in painting.brush_strokes:
+            new_color = discretize_color(brush_stroke, discrete_colors)
+            brush_stroke.color_transform.data *= 0
+            brush_stroke.color_transform.data += new_color
 
 def discretize_color(brush_stroke, discrete_colors):
     with torch.no_grad():
@@ -431,7 +309,7 @@ def discretize_color(brush_stroke, discrete_colors):
 from clip_loss import clip_conv_loss
 
 loss_l1 = torch.nn.L1Loss()
-def loss_fcn(painting, target, use_clip_loss=True):
+def loss_fcn(painting, target, use_clip_loss=False):
     loss = 0 
     #return loss_l1(painting[:,:3], target)
     diff = torch.abs(painting[:,:3] - target)
@@ -439,12 +317,12 @@ def loss_fcn(painting, target, use_clip_loss=True):
     loss += diff.mean()
 
     if use_clip_loss:
-        loss += clip_conv_loss(painting, target)
+        loss += clip_conv_loss(painting, target) * 0.25
     return loss
 
 
-def plan_all_strokes(opt, optim_iter=200, num_strokes=50, num_passes=2):
-    global strokes_small, strokes_full
+def plan_all_strokes(opt, optim_iter=100, num_strokes=120, num_passes=3):
+    global strokes_small, strokes_full, target
     strokes_small = load_brush_strokes(opt, scale_factor=5)
     strokes_full = load_brush_strokes(opt, scale_factor=1)
     
@@ -476,13 +354,15 @@ def plan_all_strokes(opt, optim_iter=200, num_strokes=50, num_passes=2):
 
         # Optimize all brush strokes
         print('Optimizing all {} brush strokes'.format(str(len(painting.brush_strokes))))
-        optim = torch.optim.Adam(painting.parameters(), lr=5e-3)
+        optim = torch.optim.Adam(painting.parameters(), lr=5e-3 * (len(painting.brush_strokes)/100))
         for j in tqdm(range(optim_iter)):
             optim.zero_grad()
             p = painting(strokes=strokes_small)
             loss = 0
-            loss += loss_fcn(p, target)
+            loss += loss_fcn(p, target,  use_clip_loss=False)
             loss.backward()
+            # for bs in painting.brush_strokes:
+            #     bs.color_transform.grad.data *= 0. # Don't change the color because CLIP sucks at color
             optim.step()
             log_progress(painting)
 
@@ -497,14 +377,14 @@ def plan_all_strokes(opt, optim_iter=200, num_strokes=50, num_passes=2):
 
         # discretize_colors(painting, colors)
 
-        # # Remove unnecessary brush strokes
-        # print("Removing unnecessary brush strokes")
-        # n_strokes_before = len(painting.brush_strokes)
-        # painting = purge_extraneous_brush_strokes(painting, target)
-        # print('Removed {} brush strokes. {} total now.'.format(str(len(painting.brush_strokes) - n_strokes_before), str(len(painting.brush_strokes))))
-        # # with torch.no_grad():
-        # #     show_img(painting(strokes=strokes_full))
-        # #     print(nn.L1Loss()(painting(strokes=strokes_small)[:,:3], target))
+        # Remove unnecessary brush strokes
+        print("Removing unnecessary brush strokes")
+        n_strokes_before = len(painting.brush_strokes)
+        painting = purge_extraneous_brush_strokes(painting, target)
+        print('Removed {} brush strokes. {} total now.'.format(str(len(painting.brush_strokes) - n_strokes_before), str(len(painting.brush_strokes))))
+        # with torch.no_grad():
+        #     show_img(painting(strokes=strokes_full))
+        #     print(nn.L1Loss()(painting(strokes=strokes_small)[:,:3], target))
 
         discretize_colors(painting, colors)
 
@@ -517,15 +397,17 @@ def plan_all_strokes(opt, optim_iter=200, num_strokes=50, num_passes=2):
 from options import Options
 from tensorboard import TensorBoard
 writer = None
+target = None
 local_it = 0 
 
-def log_progress(painting):
+def log_progress(painting, force_log=False):
     global local_it
     local_it +=1
-    if local_it %5==0:
+    if (local_it %5==0) or force_log:
         with torch.no_grad():
             np_painting = painting(strokes=strokes_small).detach().cpu().numpy()[0].transpose(1,2,0)
             opt.writer.add_image('images/planasdfasdf', np.clip(np_painting, a_min=0, a_max=1), local_it)
+            opt.writer.add_scalar('loss/plan_all_strokes_local', loss_fcn(painting(strokes=strokes_small), target).item(), local_it)
 
 def log_painting(painting, step, name='images/plan_all_strokes'):
     np_painting = painting(strokes=strokes_small).detach().cpu().numpy()[0].transpose(1,2,0)
