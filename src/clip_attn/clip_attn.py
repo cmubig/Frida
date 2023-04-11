1217fba6520349e3cb33992fb22c37b96f3a1393
pschaldenbrand <pls21@pitt.edu>
2023-04-11 13:44:40 -0400

    Forgot clip_attn attention mechanism

---
+++ src/clip_attn/clip_attn.py
@@ -0,0 +1,98 @@
+# https://github.com/hila-chefer/Transformer-MM-Explainability
+
+import torch
+import clip_attn.clip as clip
+from PIL import Image
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+from torchvision import transforms
+
+start_layer =  -1
+start_layer_text =  -1
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+attn_model = None
+
+
+clip_norm = transforms.Compose([
+    transforms.Resize((224,224), antialias=True),
+    transforms.Normalize((0.48145466, 0.4578275, 0.40821073), (0.26862954, 0.26130258, 0.27577711))
+])
+
+def get_attention(img):
+    global attn_model
+    if attn_model is None:
+        attn_model, _  = clip.load("ViT-B/32", device=device, jit=False)
+    texts = ["a man with eyeglasses"]
+    text = clip.tokenize(texts).to(device)
+
+    res_img = clip_norm(img)
+    R_text, image_relevance = interpret(model=attn_model, image=res_img, texts=text, device=device)
+
+    dim = int(image_relevance.numel() ** 0.5)
+    image_relevance = image_relevance.reshape(1, 1, dim, dim)
+    image_relevance = torch.nn.functional.interpolate(image_relevance, size=224, mode='bilinear')
+    image_relevance = image_relevance.reshape(224, 224).to(device).data.cpu().numpy()
+    image_relevance = (image_relevance - image_relevance.min()) / (image_relevance.max() - image_relevance.min())
+    
+    return image_relevance
+
+def interpret(image, texts, model, device, start_layer=start_layer, start_layer_text=start_layer_text):
+    batch_size = texts.shape[0]
+    images = image.repeat(batch_size, 1, 1, 1)
+    logits_per_image, logits_per_text = model(images, texts)
+    probs = logits_per_image.softmax(dim=-1).detach().cpu().numpy()
+    index = [i for i in range(batch_size)]
+    one_hot = np.zeros((logits_per_image.shape[0], logits_per_image.shape[1]), dtype=np.float32)
+    one_hot[torch.arange(logits_per_image.shape[0]), index] = 1
+    one_hot = torch.from_numpy(one_hot).requires_grad_(True)
+    one_hot = torch.sum(one_hot.to(device) * logits_per_image)
+    model.zero_grad()
+
+    image_attn_blocks = list(dict(model.visual.transformer.resblocks.named_children()).values())
+
+    if start_layer == -1: 
+      # calculate index of last layer 
+      start_layer = len(image_attn_blocks) - 1
+    
+    num_tokens = image_attn_blocks[0].attn_probs.shape[-1]
+    R = torch.eye(num_tokens, num_tokens, dtype=image_attn_blocks[0].attn_probs.dtype).to(device)
+    R = R.unsqueeze(0).expand(batch_size, num_tokens, num_tokens)
+    for i, blk in enumerate(image_attn_blocks):
+        if i < start_layer:
+          continue
+        grad = torch.autograd.grad(one_hot, [blk.attn_probs], retain_graph=True)[0].detach()
+        cam = blk.attn_probs.detach()
+        cam = cam.reshape(-1, cam.shape[-1], cam.shape[-1])
+        grad = grad.reshape(-1, grad.shape[-1], grad.shape[-1])
+        cam = grad * cam
+        cam = cam.reshape(batch_size, -1, cam.shape[-1], cam.shape[-1])
+        cam = cam.clamp(min=0).mean(dim=1)
+        R = R + torch.bmm(cam, R)
+    image_relevance = R[:, 0, 1:]
+
+    
+    text_attn_blocks = list(dict(model.transformer.resblocks.named_children()).values())
+
+    if start_layer_text == -1: 
+      # calculate index of last layer 
+      start_layer_text = len(text_attn_blocks) - 1
+
+    num_tokens = text_attn_blocks[0].attn_probs.shape[-1]
+    R_text = torch.eye(num_tokens, num_tokens, dtype=text_attn_blocks[0].attn_probs.dtype).to(device)
+    R_text = R_text.unsqueeze(0).expand(batch_size, num_tokens, num_tokens)
+    for i, blk in enumerate(text_attn_blocks):
+        if i < start_layer_text:
+          continue
+        grad = torch.autograd.grad(one_hot, [blk.attn_probs], retain_graph=True)[0].detach()
+        cam = blk.attn_probs.detach()
+        cam = cam.reshape(-1, cam.shape[-1], cam.shape[-1])
+        grad = grad.reshape(-1, grad.shape[-1], grad.shape[-1])
+        cam = grad * cam
+        cam = cam.reshape(batch_size, -1, cam.shape[-1], cam.shape[-1])
+        cam = cam.clamp(min=0).mean(dim=1)
+        R_text = R_text + torch.bmm(cam, R_text)
+    text_relevance = R_text
+   
+    return text_relevance, image_relevance
