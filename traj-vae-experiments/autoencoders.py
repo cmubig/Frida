058348b7fead7f92397e80fb617ae881f4d45d83
ldcWV <ldorianchen@gmail.com>
2023-11-12 17:25:08 -0500

    Implement autoencoder

--- traj-vae-experiments/autoencoders.py
+++ traj-vae-experiments/autoencoders.py
@@ -1,15 +1,65 @@
+import numpy as np
 import torch
 import torch.nn as nn
 
-class LSTM_MLP(nn.Module):
-    # LSTM encoder with MLP decoder
-    def __init__(self, input_size, hidden_size, output_size):
-        super(LSTM_MLP, self).__init__()
-        self.hidden_size = hidden_size
-        self.lstm = nn.LSTM(input_size, hidden_size)
-        self.fc = nn.Linear(hidden_size, output_size)
+class MLP_VAE(nn.Module):
+    def __init__(self, input_points_per_traj, latent_dim, output_points_per_traj):
+        super(MLP_VAE, self).__init__()
 
-    def forward(self, input):
-        lstm_out, _ = self.lstm(input)
-        output = self.fc(lstm_out[-1])
-        return output
+        self.latent_dim = latent_dim
+        
+        # Encoder
+        self.encoder = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(input_points_per_traj*3, 256),
+            nn.LeakyReLU(),
+            nn.Linear(256, 128),
+            nn.LeakyReLU()
+        )
+        self.mean_fc = nn.Linear(128, latent_dim)
+        self.logvar_fc = nn.Linear(128, latent_dim)
+
+        # Decoder
+        self.decoder = nn.Sequential(
+            nn.Linear(latent_dim, 128),
+            nn.LeakyReLU(),
+            nn.Linear(128, 256),
+            nn.LeakyReLU(),
+            nn.Linear(256, output_points_per_traj*3),
+            nn.Unflatten(1, (output_points_per_traj, 3)),
+            nn.Tanh()
+        )
+
+        self.dummy_param = nn.Parameter(torch.empty(0))
+    
+    def encode(self, trajectory):
+        encoded = self.encoder(trajectory)
+        mean = self.mean_fc(encoded)
+        logvar = self.logvar_fc(encoded)
+        return mean, logvar
+
+    def reparameterize(self, mean, logvar):
+        std = torch.exp(0.5 * logvar)
+        eps = torch.randn_like(std)
+        z = mean + eps * std
+        return z
+    
+    def decode(self, z):
+        decoded = self.decoder(z)
+        return decoded
+    
+    def forward(self, trajectory):
+        mean, logvar = self.encode(trajectory)
+        z = self.reparameterize(mean, logvar)
+        decoded = self.decode(z)
+        return decoded, mean, logvar
+    
+    def sample_trajectories(self, n):
+        batch_size = 16
+        res = []
+        for i in range(0, n, batch_size):
+            z = torch.randn(min(batch_size, n - i), self.latent_dim)
+            z = z.to(self.dummy_param.device)
+            res.append(self.decode(z).detach().cpu().numpy())
+        res = np.concatenate(res, axis=0)
+        return res
