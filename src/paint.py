fix hacky stroke batches + freezing; instead, randomly sample strokes to compute gradients for each iteration

src/paint.py
@@ -69,44 +69,13 @@ if __name__ == '__main__':
     load_objectives_data(opt)
 
     if opt.overwrite_painting or opt.painting_path is None or not os.path.exists(opt.painting_path):
-        # Optimize strokes one batch at a time, freezing strokes from previous batches
-        strokes = []
-        current_alphas = None
-        # num_batches = (opt.num_strokes // opt.strokes_per_batch) + (1 if opt.num_strokes % opt.strokes_per_batch > 0 else 0)
-        num_batches = 1
-        for batch_ind in range(num_batches):
-            # num_strokes = min(opt.strokes_per_batch, opt.num_strokes - batch_ind * opt.strokes_per_batch)
-            num_strokes = opt.num_strokes
+        painting = random_init_painting(opt, current_canvas, opt.num_strokes, ink=opt.ink)
+        painting.to(device)
 
-            painting = random_init_painting(opt, current_canvas, num_strokes, ink=opt.ink)
-            painting.to(device)
-
-            if color_palette is not None:
-                discretize_colors(painting, color_palette)
-            
-            painting, color_palette = optimize_painting(
-                opt, painting, 
-                optim_iter=opt.init_optim_iter,
-                color_palette=color_palette,
-                preexisting_alphas=current_alphas,
-                fill_weight = opt.fill_weight if num_batches == 1 else opt.fill_weight * batch_ind / (num_batches-1)
-            )
-
-            current_canvas, alphas = painting(h_render, w_render, use_alpha=False, return_alphas=True)
-            alphas = alphas.clone().detach()
-            if current_alphas is None:
-                current_alphas = alphas
-            else:
-                current_alphas = torch.max(current_alphas, alphas)
-            for bs in painting.brush_strokes:
-                strokes.append(copy.deepcopy(bs))
-
-            painting = None
-            gc.collect()
-            with torch.no_grad():
-                torch.cuda.empty_cache()
-
-        painting = Painting(opt, brush_strokes=strokes)
+        if color_palette is not None:
+            discretize_colors(painting, color_palette)
+        
+        painting, color_palette = optimize_painting(opt, painting, optim_iter=opt.init_optim_iter, color_palette=color_palette)
 
         # Save painting to file
         if opt.painting_path is not None:

src/painting.py
@@ -62,8 +62,9 @@ class Painting(nn.Module):
         stroke_alphas = None
         if return_alphas: stroke_alphas = []
 
-        def apply_stroke(brush_stroke, canvas, stroke_alphas):
+        def apply_stroke(brush_stroke, canvas, stroke_alphas, compute_gradients):
             single_stroke = brush_stroke(h,w, self.param2img)
+            if not compute_gradients: single_stroke = single_stroke.detach()
 
             if adjust_extreme_alphas:
                 single_stroke[:,3][single_stroke[:,3] > 0.5] = 1.
@@ -87,10 +88,9 @@ class Painting(nn.Module):
 
         for i, brush_stroke in enumerate(self.brush_strokes):
             if use_grad[i]:
-                canvas, stroke_alphas = apply_stroke(brush_stroke, canvas, stroke_alphas)
+                canvas, stroke_alphas = apply_stroke(brush_stroke, canvas, stroke_alphas, True)
             else:
-                with torch.no_grad():
-                    canvas, stroke_alphas = apply_stroke(brush_stroke, canvas, stroke_alphas)
+                canvas, stroke_alphas = apply_stroke(brush_stroke, canvas, stroke_alphas, False)
         
         if return_alphas: 
             alphas = torch.cat(stroke_alphas, dim=1)

src/painting_optimization.py
@@ -130,7 +130,7 @@ def load_objectives_data(opt):
     opt.objective_data_loaded = objective_data
 
 def optimize_painting(opt, painting, optim_iter, color_palette=None,
-                      change_color=True, shuffle_strokes=True, preexisting_alphas=None, fill_weight=0.0):
+                      change_color=True, shuffle_strokes=True):
     """
     kwargs:
         color_palette: if None, then it creates a new one
@@ -157,8 +157,6 @@ def optimize_painting(opt, painting, optim_iter, color_palette=None,
                 optims[i_o].param_groups[0]['lr'] = og_lrs[i_o]*lr_factor
 
         p, alphas = painting(opt.h_render, opt.w_render, use_alpha=False, return_alphas=True)
-        if preexisting_alphas is not None:
-            alphas = torch.max(preexisting_alphas, alphas)
         
         loss = 0
         for k in range(len(opt.objective)):
@@ -167,8 +165,8 @@ def optimize_painting(opt, painting, optim_iter, color_palette=None,
                 weight=opt.objective_weight[k],
                 num_augs=opt.num_augs)
         #loss += (1-alphas).mean() * opt.fill_weight
-        if fill_weight > 0:
-            loss += torch.abs(1-alphas).mean() * fill_weight
+        if opt.fill_weight > 0:
+            loss += torch.abs(1-alphas).mean() * opt.fill_weight
         loss.backward()
 
         for o in optims: o.step() if o is not None else None